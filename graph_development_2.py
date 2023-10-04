74aa6e1ccbc765e39c9779fc1efb581e5ef08ac9
Landry-G <gabriel.landry@live.ca>
2023-10-04 15:46:42 -0400

    New Factory

--- graph_development_2.py
+++ graph_development_2.py
@@ -60,6 +60,9 @@ class RecursionNode(RecursionError):
 
 # Node class -----------------------------------------------------------------
 
+class WPGraph():
+    pass
+
 class IndustrialNode(metaclass = ABCMeta): # aller voir la doc ABC
     def __init__(self, LOCALNAME: str):
         self._NAME = LOCALNAME
@@ -76,7 +79,7 @@ class IndustrialNode(metaclass = ABCMeta): # aller voir la doc ABC
        raise ConstError("Node name can't be changed")
     
     @abstractmethod
-    def GetCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def GetCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         ''' Get Carbon Documentation
         
         Cette fonction sert à retracer le flux de carbone parcourant le réseau
@@ -112,17 +115,21 @@ class TopNode(IndustrialNode):
         except ValueError:
             return 0
     
-    def GetCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def GetCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         return self._GetQuantityTime(Time)
         
 class ProportionNode(IndustrialNode):
     def __init__(self, NAME: str):
         super().__init__(NAME)
+        
+    #  À y revenir
+    #def _GutsofGetCarbon(self, Proportion: list[float], Values: list[float]) -> float:
+        
     
-    def GetCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def GetCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         Total = 0
-        for Parent in Graph.predecessors(self):
-            ProportionParent = self._GetValueTime(Graph.get_edge_data(Parent, self)["Proportion"], Time)
+        for Parent in Graph.GetPredecessors(self):
+            ProportionParent = self._GetValueTime(Graph.GetEdgeProportions(Parent, self), Time)
             if ProportionParent == 0:
                 continue
             ParentCarbon = Parent.GetCarbon(Graph, Time, Cumulative) # Récursivité
@@ -142,36 +149,63 @@ class DecayNode(ProportionNode):
     def HalfLife(self, Value):
        self._HalfLife = Value
     
-    def GetCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def GetCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         Total = 0
         for Year in range(Time + 1): 
-            if Year == Time:
-                Total += 0
-            else:    
+            if Year != Time:   
                 Annual = super().GetCarbon(Graph, Year, Cumulative)
                 Output_annual = (Annual - (Annual * ((0.5) ** ((Time - Year)/self.HalfLife)))) - \
                     (Annual - (Annual * ((0.5) ** ((Time - Year - 1)/self.HalfLife))))
                 Total += Output_annual
         return Total
+    
+    def CountCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
+        ''' CountCarbon Documentation
+
+        La fonction CountCarbon sert à faire le cumulatif des flux annuels 
+        de 0 jusqu'à l'année demandé pour un noeud en particulier grâce à la
+        fonction GetCarbon. En d'autres mots, la fonction CountCarbon sert
+        à calculer les stocks présent dans un noeud grâce à l'ensemble
+        des flux en provenance des nodes parents depuis le début.
+
+        Args: 
+            Graph (nx.DiGraph): le DiGraph utilisé pour construire le réseau
+            Time (int): Le temps en année
+            Cumulative (bool): To be implemented
+
+        Returns: 
+            float: Le cumulatif des fluxs, donc le stock de carbone présent
+            dans une noeud (self) à un temps donnée (Time)
+
+        '''
+        try:
+            Total = 0
+            for Year in range(Time + 1):    
+                Annual = super().GetCarbon(Graph, Year, Cumulative)
+                Restant = Annual * ((0.5) ** ((Time - Year)/self.HalfLife))
+                Total += Restant
+            return Total
+        except RecursionError:
+            raise  RecursionNode("Un maximum de demande a été effectué. \
+                                 Une boucle entre des ProportionNode est présente")          
+
 
 class RecyclingNode(ProportionNode):
     def __init__(self, NAME: str):
         super().__init__(NAME)
     
-    def GetCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def GetCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         Total = 0
         for Year in range(Time + 1): 
             if Year + 1 == Time:
                 Total += super().GetCarbon(Graph, Year, Cumulative)
-            else:    
-                Total += 0 # Pour Guillaume
         return Total
 
 class PoolNode(ProportionNode):
     def __init__(self, NAME):
         super().__init__(NAME)
         
-    def CountCarbon(self, Graph: nx.DiGraph, Time: int, Cumulative: bool = True) -> float:
+    def CountCarbon(self, Graph: WPGraph, Time: int, Cumulative: bool = True) -> float:
         ''' CountCarbon Documentation
         
         La fonction CountCarbon sert à faire le cumulatif des flux annuels 
@@ -202,63 +236,109 @@ class PoolNode(ProportionNode):
 #test1 = nx.DiGraph()
 #
 #A = TopNode('A', [0], [10])  
-#B = ProportionNode('B')
-#C = RecyclingNode('C')
-#D = ProportionNode('D')
-##E = DecayNode('E', 20)
-##F = ProportionNode('F')
-#G = PoolNode('G')
-#
-#test1.add_node(TopNode('A', [0], [10]))
-#
+#B = DecayNode('B', 3)
+#C = DecayNode('C', 3)
+#D = DecayNode('D', 3)
+#E = RecyclingNode('E')
+#F = PoolNode('F')
+###F = ProportionNode('F')
+##G = PoolNode('G')
+##
+##test1.add_node(TopNode('A', [0], [10]))
+##
 #test1.add_node(A)
 #test1.add_node(B)
 #test1.add_node(C)
 #test1.add_node(D)
-#test1.add_node(G)
+#test1.add_node(E)
+#test1.add_node(F)
+##test1.add_node(G)
+##
+#test1.add_edge(A, B, Proportion = [1])
+#test1.add_edge(B, C, Proportion = [1])
+#test1.add_edge(C, D, Proportion = [0.5])
+#test1.add_edge(C, E, Proportion = [0.5])
+#test1.add_edge(E, B, Proportion = [1])
+#test1.add_edge(D, F, Proportion = [1])
 #
-#test1.add_edge(A, B, Proportion = 1)
-#test1.add_edge(B, C, Proportion = 1)
-#test1.add_edge(C, D, Proportion = 1)
-#test1.add_edge(D, G, Proportion = 0.5)
+#total = 0
+#for i in test1.nodes():
+#    if type(i) == PoolNode:
+#        total += i.CountCarbon(test1, 16)
+#    if type(i) == DecayNode:
+#        total += i.CountCarbon(test1, 16)
+#    if type(i) == RecyclingNode:
+#        total += i.GetCarbon(test1, 17)
+#    print(total)
+#
 
 # Factory -------------------------------------------------------------------- 
 
-class Edge(): 
-    def __init__(self, RECYCLING: bool, VALUES: list[float], OVERFLOW: bool):
-        self.__RECYCLING__ = RECYCLING
-        self.__VALUES__ = VALUES
-        self.__OVERFLOW__ = OVERFLOW
-        
-    @property
-    def RECYCLING(self):
-        return self.__RECYCLING__
+#class Edge(): 
+#    def __init__(self, RECYCLING: int, VALUES: list[float], OVERFLOW: bool):
+#        self._RECYCLING = RECYCLING
+#        self._VALUES = VALUES
+#        self._OVERFLOW = OVERFLOW
+#        
+#    @property
+#    def RECYCLING(self):
+#        return self._RECYCLING
+#    
+#    @RECYCLING.setter
+#    def RECYCLING(self, input):
+#       raise ConstError("Original value from Miro can't be changed")
+#   
+#    @property
+#    def OVERFLOW(self):
+#        return self._OVERFLOW
+#    
+#    @OVERFLOW.setter
+#    def OVERFLOW(self, input):
+#       raise ConstError("Original value from Miro can't be changed")
+#   
+#    def GetValueTime(self, Time: int) -> float:
+#        return self._VALUES[min(Time, len(self._VALUES) - 1)]
     
-    @RECYCLING.setter
-    def RECYCLING(self, input):
-       raise ConstError("Original value from Miro can't be changed")
-   
-    @property
-    def OVERFLOW(self):
-        return self.__OVERFLOW__
-    
-    @OVERFLOW.setter
-    def OVERFLOW(self, input):
-       raise ConstError("Original value from Miro can't be changed")
-   
-    def GetValueTime(self, Time: int) -> float:
-        return self.__VALUES__[min(Time, len(self.__VALUES__) - 1)]
-
-class GraphFactory(): 
+class GraphFactory2(): 
     def __init__(self, DIR: str):
         self._DIRECTORY = DIR
         with open(self._DIRECTORY, "r") as files: 
             self._DATA = json.load(files)
         self._GRAPHNAME = []
         self._GRAPHS = []
-        for KEY, VALUES in self._DATA.items():
+        
+        Graph_num = 0
+        keys = list(self.GetData.keys())
+        keys.sort()
+        for KEY in keys:
             self._GRAPHNAME.append(KEY.upper())
-            self._GRAPHS.append(WPGraph(KEY.upper(), VALUES))
+            self._GRAPHS.append(WPGraph2(KEY))
+            _EDGES = self.GetData[KEY].get('Edges', {})
+            _NODES = self.GetData[KEY].get('Nodes', {})
+            _TOPNODES = set([int(ID) for ID in _NODES]) - \
+                set([data['To'] for keys, data in _EDGES.items()])
+            _LASTNODES = set([int(ID) for ID in _NODES]) - \
+                set([data['From'] for keys, data in _EDGES.items()])
+        
+            node_map = {}
+            for node_id, node_data in _NODES.items():
+                if int(node_id) in _TOPNODES:    
+                    new_node =  TopNode(node_data['Name'], [0], [10])  # Reste à mettre le input
+                elif int(node_id) in _LASTNODES:
+                    new_node = PoolNode(node_data['Name'])
+                elif node_data["Half-life"] > 0:
+                    new_node = DecayNode(node_data['Name'], node_data['Half-life'])
+                elif node_data['Recycling'] == 1: 
+                    new_node = RecyclingNode(node_data['Name'])
+                else: new_node = ProportionNode(node_data['Name'])
+                self._GRAPHS[Graph_num].AddNode(new_node)
+                node_map[int(node_id)] = new_node        
+
+            for edge_id, edge_data in _EDGES.items():
+                From = node_map[edge_data['From']]
+                To = node_map[edge_data['To']]
+                self._GRAPHS[Graph_num].AddEdge(From, To, edge_data['Values'])  
+            Graph_num += 1     
 
     @property
     def GetGraphName(self) -> list:
@@ -266,7 +346,7 @@ class GraphFactory():
     
     @GetGraphName.setter
     def GetGraphName(self, input):
-        raise ConstError("Graph name can't be changed")
+        raise ConstError("Graph name can't be changed outside Miro")
     
     @property
     def GetData(self):
@@ -274,50 +354,63 @@ class GraphFactory():
     
     @GetData.setter
     def GetData(self, input):
-        raise ConstError("Data from graphs can't be changed")
-    
-    def GetNodes(self, GraphName: str):
-        return self._DATA[GraphName].get('Nodes', {})
-    
-    def GetEdges(self, GraphName: str):
-        return self._DATA[GraphName].get('Edges', {})
-        
-class WPGraph():
-    def __init__(self, KEY, VALUES):
-        self._GRAPH = nx.DiGraph()
+        raise ConstError("Data from graphs can't be changed outside Miro")  
+   
+class WPGraph2():
+    def __init__(self, KEY):
+        super().__init__()
+        self._Graph = nx.DiGraph()
         self._NAME = KEY
-        _EDGES = VALUES.get('Edges', {})
-        _NODES = VALUES.get('Nodes', {})
-        _TOPNODE = set([int(ID) for ID in _NODES]) - \
-            set([data['To'] for keys, data in _EDGES.items()])
-        _ENDNODE = set([int(ID) for ID in _NODES]) - \
-            set([data['From'] for keys, data in _EDGES.items()])
-        
-        node_map = {}
-        
-        # Prendre le foor loop et le mettre dans le Factory?
-        
-        for node_id, node_data in _NODES.items():
-            new_node = ProportionNode(node_data['Name'])
-            if int(node_id) in _TOPNODE:    
-                new_node =  TopNode(node_data['Name'], [0], [10])   
-            elif int(node_id) in _ENDNODE:
-                new_node = PoolNode(node_data['Name'])
-            elif node_data["Half-life"] > 0:
-                new_node = DecayNode(node_data['Name'], node_data['Half-life'])
-            elif node_data['Recycling'] == 1: 
-                new_node = RecyclingNode(node_data['Name'])
-            self._GRAPH.add_node(new_node)
-            node_map[int(node_id)] = new_node        
+    
+    def AddNode(self, node):
+        return self._Graph.add_node(node)
+    
+    def AddEdge(self, From, To, Proportion_value):
+        return self._Graph.add_edge(From, To, Proportion = Proportion_value)
+    
+    def GetEdgeProportions(self, From: IndustrialNode, To: IndustrialNode) -> list[float]:
+        return self._Graph.get_edge_data(From, To)["Proportion"]
+    
+    def GetPredecessors(self, Node: IndustrialNode) -> list[IndustrialNode]:
+        return [Parent for Parent in self._Graph.predecessors(Node)]
+    
+    def Nodes(self):
+        return self._Graph.nodes()
+    
+    @property
+    def GetGraphName(self):
+        return self._NAME
+    
+    @GetGraphName.setter
+    def GetGraphName(self):
+        return ConstError("Graph name can't be changed outside Miro")
+       
+test3 = GraphFactory2('C:/Users/langa3/Documents/Script/Panier_produit/Graphs.json')
 
-        for edge_id, edge_data in _EDGES.items():
-            From = node_map[edge_data['From']]
-            To = node_map[edge_data['To']]
-            self._GRAPH.add_edge(From, To, Proportion = edge_data['Values'])
+#for i in test3._GRAPHS[0].nodes():
+#    print(i)
 
+total = 0
+
+for i in test3._GRAPHS[0].Nodes():
+    if type(i) == PoolNode:
+        #print(f"La node {i._NAME} contient {i.CountCarbon(test3._GRAPHS[0], 10)} à l'année 10")
+        total += i.CountCarbon(test3._GRAPHS[0], 10)
+    if type(i) == DecayNode:
+        #print(f"La node {i._NAME} contient {i.CountCarbon(test3._GRAPHS[0], 10)} à l'année 10")
+        total += i.CountCarbon(test3._GRAPHS[0], 10)
+        print(total)
         
-test2 = GraphFactory('C:/Users/langa3/Documents/Script/Panier_produit/Graphs.json')
-
+        
+#total = 0
+#for i in test3._GRAPHS[0].nodes():
+#    if type(i) == PoolNode:
+#        total += i.CountCarbon(test3._GRAPHS[0], 10)
+#    elif type(i) == DecayNode:
+#        total += i.CountCarbon(test3._GRAPHS[0], 10)
+#    print(total)
+    
+    
 #nx.draw(test2._GRAPHS[0]._GRAPH)
 #with open('C:/Users/langa3/Documents/Script/Panier_produit/Graphs.json', "r") as files:
 #    data = json.load(files)
@@ -338,11 +431,7 @@ test2 = GraphFactory('C:/Users/langa3/Documents/Script/Panier_produit/Graphs.jso
 #        elif int(node_id) == To:
 #            nom_to = node_data['Name']
 #    print(f'Connection entre {nom_from} à {nom_to}')
-   
-for i in test2._GRAPHS[0]._GRAPH.nodes(): 
-    if type(i) == PoolNode:
-        print(f"La node {i._NAME} contient {i.CountCarbon(test2._GRAPHS[0]._GRAPH, 10)} à l'année 10")
-
+ 
 # Test section --------------------------------------------------------------- 
 
 # class CONST(object):
@@ -356,56 +445,3 @@ for i in test2._GRAPHS[0]._GRAPH.nodes():
 # CONST_1.FOO = 4321              # AttributeError: 'CONST' object attribute 'FOO' is read-only
 # CONST_1.__dict__['FOO'] = 4321  # AttributeError: 'CONST' object has no attribute '__dict__'
 # CONST_1.BAR = 5678 
-
-# Old code -------------------------------------------------------------------
-# G = nx.Graph()
-# p0 = point(0,0)
-# p1 = point(1,1)
-# 
-# G.add_node(0, data=p0)
-# G.add_node(1, data=p1)
-# G.add_edge(0,1, weight=4)
-# G[0]
-# AtlasView({1: {'weight': 4}})  #in networkx 1.x this is actually a dict. In 2.x it is an "AtlasView"
- 
-# class GraphFactory(): # Intrant sera un JSON file
-#     def __init__(self, DIR: str):
-#         self._DIRECTORY = DIR
-#         with open(DIR, "r") as files: 
-#             self.__DATA__ = json.load(files)
-#         nom = []
-#         self.__GRAPS = []
-#         for GRAPH in self.__DATA__:
-#             nom.append(GRAPH.upper())
-#             #setattr(self, f"__{GRAPH.upper()}__", self.__DATA__[GRAPH])
-#             #setattr(self, f"__{GRAPH.upper()}_NODES__", self.__DATA__[GRAPH].get('Nodes', {}))
-#             #setattr(self, f"__{GRAPH.upper()}_EDGES__", self.__DATA__[GRAPH].get('Edges', {}))
-#             setattr(self, f"__{GRAPH.upper()}__", nx.DiGraph())
-#             NODES = self.__DATA__[GRAPH].get('Nodes', {}).items()
-#             EDGES = self.__DATA__[GRAPH].get('EDGES', {}).items()
-#             
-#             NODES_ID = [ID for ID in NODES]
-#             TO_EDGES = [edge_data['To'] for edge_data in EDGES.values()]
-#             
-#             FIRSTNODE = set(NODES_ID) - set(TO_EDGES)
-#             
-#             for node_id, node_data in NODES:
-#                 setattr(self, f"_{node_id}", ProportionNode(node_data['Name']))
-#                 getattr(self, f"__{GRAPH.upper()}__").add_node(f"_{node_id}")
-#             for edge_id, edge_data in EDGES:
-#                 getattr(self, f"__{GRAPH.upper()}__").add_edge(edge_data['From'], 
-#                                                                edge_data['To'], 
-#                                                                Proportion = edge_data['Values'])
-# 
-
-# setattr(self._GRAPH, f"_{node_id}", DecayNode(node_data['Name']))
-
-#test = [0, 1, 2, 3, 4, 5, 6]
-#
-#for i in test:
-#    while i < 4:
-#        print(f'{i} est en bas de 4')
-#        break
-#    else: 
-#        print('yo')
-#        break
