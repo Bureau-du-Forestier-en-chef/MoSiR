57d9da8ff6a97f86e29228e586e9363549cb9fd9
Landry-G <gabriel.landry@live.ca>
2023-10-23 09:03:14 -0400

    update

--- ImportFormatting.py
+++ ImportFormatting.py
@@ -5,33 +5,41 @@ import pandas as pd
 class EditImport():
     def __init__(self, directory):
         self._DIR = directory
+        self._INPUT = []
+        self._UNIT = []
         with open(directory, 'r') as txt:
             Saved = False
-            Input = []
             Bracket = 0
-            Unit = []
             # Carbon input
             for line in txt.readlines():
                 if line.startswith('#'):
                     continue
-                if line.startswith('Unit:') and 'kgC' or line.startswith('Unit:') and 'tC':
-                    Unit.append(line.split(':', 1))
+                if line.startswith('Unit:'):
+                    Unit = line.strip().split(':', 1)
+                    self._UNIT.append(Unit[1])
                 if line.startswith('CarbonInputs'):
                     Saved = True
                     Parts = line.split('{', 1)
-                if len(Parts) > 1:
-                    Input.append('{' + Parts[1].strip())
+                    if len(Parts) > 1:
+                        self._INPUT.append('{' + Parts[1].strip())
                 elif Saved:
-                    Input.append(line.strip())
+                    self._INPUT.append(line.strip())
                 if Saved and '{' in line:
                     Bracket += 1
                 elif Saved and '}' in line:
                     Bracket -= 1
                 if Bracket == 0:
                     Saved = False
-    # Unit
+    def GetInput(self):
+        return eval(''.join(self._INPUT))
+    def GetUnit(self):
+        Unit = self._UNIT[0].strip().lower()
+        assert Unit in ['kgc', 'tc']
+        return Unit
     
-    
-Unit 
-Input = eval(''.join(Input))
-Input
+Test01 = EditImport('Import.txt')               
+Test01.GetUnit()
+Test01._UNIT
+
+for i in Test01._UNIT:
+    print(i)
