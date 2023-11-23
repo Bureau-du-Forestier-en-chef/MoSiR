db1ea673a1e5055d158d479cb01b6406df923062
Gabriel Landry <65407496+Landry-G@users.noreply.github.com>
2023-11-23 10:21:17 -0500

    Rad change test

--- MoSiR/__init__.py
+++ MoSiR/__init__.py
@@ -10,8 +10,8 @@ from .blueprint_component import Component as Component
 from .views import Flaskwrapper as Flaskwrapper
 from . import GraphGen as GraphGen
 from . import NetworkxGraph as NetworkxGraph
-from . import graph_test as graph_test
 from . import ImportInfo as ImportInfo
 from . import ReportingInfo as ReportingInfo
 from . import CalculatorRun as CalculatorRun
 from . import mosir_exceptions as mosir_exceptions
+from . import CarbonToRad as CarbonToRad
